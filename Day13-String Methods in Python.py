0ff30b7bf3124e58d572717b91eb8e6afc64cbdc
jitkoley <jitkoley7@gmail.com>
2024-02-25 12:33:11 +0530

    day 13 added

---
+++ Day13-String Methods in Python.py
@@ -0,0 +1,15 @@
+# strings are immutable 
+a= "jitkoley"
+print(len(a))
+print(a.upper())
+# strip()
+b="Hi!, how are you?"
+print(b.rstrip("?"))
+# replace()
+print(b.replace("Hi","jit"))
+# split()
+c="hi, jit, how are you?"
+print(c.split(" "))
+# capitalize()
+blog="introduction to python"
+print(blog.capitalize())
